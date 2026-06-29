c26c759d79fcc1fdeb04d7082da6b84f31457e3e
dongthinhpt02 <trandongthinh@gmail.com>
2026-06-29 22:42:19 +0700

    chinh sua quen mat khau

--- src/shared/utils/mailer.ts
+++ src/shared/utils/mailer.ts
@@ -2,25 +2,25 @@ import nodemailer from "nodemailer";
 import appConfig from "../common/config";
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    // service: "gmail",
-    auth: {
-        user: appConfig.google.smtpGoogleEmail,
-        pass: appConfig.google.smtpGooglePassword,
-    },
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  // service: "gmail",
+  auth: {
+    user: appConfig.google.smtpGoogleEmail,
+    pass: appConfig.google.smtpGooglePassword,
+  },
 });
 
 export async function sendResetPasswordEmail(to: string, resetLink: string) {
-    await transporter.sendMail({
-        from: "Badminton Store",
-        to,
-        subject: "Reset your password",
-        html: `
+  await transporter.sendMail({
+    from: "Badminton Store",
+    to,
+    subject: "Reset your password",
+    html: `
             <p>Click the link below to reset your password:</p>
             <a href="${resetLink}">${resetLink}</a>
             <p>This link will expire in 1 hour.</p>
-        `
-    });
+        `,
+  });
 }
